f5ac9fc67194a24de6f04d33b84dca1358fd3c48
Lajeesh <gokulam.9920@gmail.com>
2024-09-04 15:00:38 +0530

    fetch data to app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,24 +2,48 @@ import React from "react";
 import { useEffect, useState } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
-import clear from "../assets/clear.png";
-import cloud from "../assets/cloud.png";
-import drizzle from "../assets/humidity.png";
-import rain from "../assets/rain.png";
-import snow from "../assets/snow.png";
-import wind from "../assets/wind.png";
-import humidity from "../assets/humidity.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/humidity.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import humidity_icon from "../assets/humidity.png";
 
 const Weather = () => {
-  const [city, setCity] = useState("London"); // Default city
+  const [city, setCity] = useState("mumbai"); // Default city
+  const [weatherData, setWeatherData] = useState(false);
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "032": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
 
   const search = async (city) => {
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=da2b4ad0632f3fa6bdb1b87a24daa4ad`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=da2b4ad0632f3fa6bdb1b87a24daa4ad`;
 
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
+      // const icon = allIcons[data.Weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+      });
     } catch (error) {
       console.error("Error fetching the weather data:", error);
     }
@@ -35,21 +59,21 @@ const Weather = () => {
         <input type="text" placeholder="Search" />
         <img src={search_icon} alt="search" />
       </div>
-      <img src={clear} alt="clear" className="weather-icon" />
-      <p className="temperature">16°c</p>
-      <p className="location">London</p>
+      <img src={clear_icon} alt="clear" className="weather-icon" />
+      <p className="temperature">{weatherData.temperature}°c</p>
+      <p className="location">{weatherData.location}</p>
       <div className="weather-data">
         <div className="col">
-          <img src={humidity} alt="wind" />
+          <img src={humidity_icon} alt="wind" />
           <div>
-            <p>91 %</p>
+            <p>{weatherData.humidity} %</p>
             <span>Humidity</span>
           </div>
         </div>
         <div className="col">
-          <img src={wind} alt="wind" />
+          <img src={wind_icon} alt="wind" />
           <div>
-            <p>3.4 km/h</p>
+            <p>{weatherData.windSpeed} km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
