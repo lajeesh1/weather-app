14dfd9322bfebb63d7dc6fea230fca5a4be7ea65
Lajeesh <gokulam.9920@gmail.com>
2024-09-09 13:34:22 +0530

    city search completed

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
 import clear_icon from "../assets/clear.png";
 import cloud_icon from "../assets/cloud.png";
-import drizzle_icon from "../assets/humidity.png";
+import drizzle_icon from "../assets/drizzle.png";
 import rain_icon from "../assets/rain.png";
 import snow_icon from "../assets/snow.png";
 import wind_icon from "../assets/wind.png";
 import humidity_icon from "../assets/humidity.png";
 
 const Weather = () => {
-  const [city, setCity] = useState("chennai"); // Default city
+  const inputRef = useRef();
+
   const [weatherData, setWeatherData] = useState(false);
   const allIcons = {
     "01d": clear_icon,
@@ -52,14 +53,18 @@ const Weather = () => {
   };
 
   useEffect(() => {
-    search(city);
+    search("london");
   }, []);
 
   return (
     <div className="weather">
       <div className="search-bar">
-        <input type="text" placeholder="Search" />
-        <img src={search_icon} alt="search" />
+        <input ref={inputRef} type="text" placeholder="Search" />
+        <img
+          onClick={() => search(inputRef.current.value)}
+          src={search_icon}
+          alt="search"
+        />
       </div>
       <img src={weatherData.icon} alt="clear" className="weather-icon" />
       <p className="temperature">{weatherData.temperature}°c</p>
